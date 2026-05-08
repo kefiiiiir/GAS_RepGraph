3721b1b61451df99b62677926a5baa92444c0de7
kefir_twtch <159289576+kefiiiiir@users.noreply.github.com>
2026-05-08 11:43:49 +0300

    Добавление способностей, пассивных способностей и их инициализация

--- Source/GAS_RepGraph/Private/GAS/GASAbilitySystemComponent.cpp
+++ Source/GAS_RepGraph/Private/GAS/GASAbilitySystemComponent.cpp
@@ -3,3 +3,44 @@
 
 #include "GAS/GASAbilitySystemComponent.h"
 
+void UGASAbilitySystemComponent::AddCharacterAbilities(const TArray<TSubclassOf<UGameplayAbility>>& AbilitiesToGrant)
+{
+	// Проходим по всем классам способностей и предоставляем их этому классу способностей.
+	for (const TSubclassOf<UGameplayAbility>& Ability : AbilitiesToGrant)
+	{
+		// Создайте базовую специализацию способностей с 1-м уровнем.
+		FGameplayAbilitySpec AbilitySpec = FGameplayAbilitySpec(Ability, 1.f);
+		
+		// Предоставляем возможность этому компоненту системы способностей.
+		GiveAbility(AbilitySpec);
+	}
+}
+
+void UGASAbilitySystemComponent::AddCharacterPassiveAbilities(
+	const TArray<TSubclassOf<UGameplayAbility>>& PassivesToGrant)
+{
+	// Пассивные способности предоставляются и активируются немедленно.
+	for (const TSubclassOf<UGameplayAbility>& Ability : PassivesToGrant)
+	{
+		FGameplayAbilitySpec AbilitySpec = FGameplayAbilitySpec(Ability, 1.f);
+		
+		// Предоставляет способность и активирует её один раз сразу после назначения.
+		GiveAbilityAndActivateOnce(AbilitySpec);
+	}
+}
+
+void UGASAbilitySystemComponent::InitializeDefaultAttributes(const TSubclassOf<UGameplayEffect>& AttributeEffect)
+{
+	// проверяем, существует ли допустимый эффект инициализации атрибута.
+	checkf(AttributeEffect, TEXT("No Valid defualt attributes for this character %s"), *GetAvatarActor()->GetName());
+	
+	// Создание контекста игрового эффекта для его самостоятельного применения
+	const FGameplayEffectContextHandle ContextHandle = MakeEffectContext();
+	
+	// Создание спецификации для эффекта инициализации атрибута.
+	const FGameplayEffectSpecHandle SpecHandle = MakeOutgoingSpec(AttributeEffect, 1.f, ContextHandle);
+	
+	// Применить эффект инициализации атрибута к самому себе.
+	ApplyGameplayEffectSpecToSelf(*SpecHandle.Data.Get());
+}
+
--- Source/GAS_RepGraph/Public/GAS/GASAbilitySystemComponent.h
+++ Source/GAS_RepGraph/Public/GAS/GASAbilitySystemComponent.h
@@ -7,11 +7,54 @@
 #include "GASAbilitySystemComponent.generated.h"
 
 /**
- * 
- */
+* Пользовательский компонент системы способностей игрового процесса.
+* Расширяет базовый компонент UAbilitySystemComponent вспомогательными функциями
+* для инициализации персонажа и настройки игрового процесса.
+
+* Обязанности:
+* - Предоставление активных способностей
+* - Предоставление пассивных способностей
+* - Применение атрибута по умолчанию GameplayEffects
+
+* Разработан для централизации логики настройки GAS и предотвращения повторений
+* для разных персонажей и классов игрового процесса.
+*/
 UCLASS()
 class GAS_REPGRAPH_API UGASAbilitySystemComponent : public UAbilitySystemComponent
 {
 	GENERATED_BODY()
 	
+public:
+	/**
+	* Предоставляет персонажу активные игровые способности.
+	
+	* Эти способности:
+	* - Активируются игроком
+	* - Сохраняются после предоставления
+	* - Привязываются к вводу или игровым событиям
+	*/
+	void AddCharacterAbilities(const TArray<TSubclassOf<UGameplayAbility>>& AbilitiesToGrant);
+	
+	/**
+	* Предоставляет пассивные игровые способности и активирует их немедленно.
+
+	* Эти способности:
+	* - Не требуют участия игрока
+	* - Активируются один раз после предоставления
+	* - Обычно обрабатывают баффы или постоянные модификаторы
+	*/
+	void AddCharacterPassiveAbilities(const TArray<TSubclassOf<UGameplayAbility>>& PassivesToGrant);
+	
+	/**
+	* Применяет атрибут GameplayEffect по умолчанию для инициализации характеристик.
+
+	* Примеры атрибутов:
+	* - Здоровье
+	* - Мана
+	* - Выносливость
+	* - Броня
+	*/
+	void InitializeDefaultAttributes(const TSubclassOf<UGameplayEffect>& AttributeEffect);
+	
 };
+
